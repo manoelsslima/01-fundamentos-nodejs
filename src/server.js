b024599f11afc4027e73d4ed5f9ddacc00568fe0
Manoel Lima <manoelsslima@yahoo.com.br>
2024-12-15 17:09:10 -0400

    feat: a little bit about buffers

--- src/server.js
+++ src/server.js
@@ -2,9 +2,25 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const { method, url } = req
+
+    const buffers = [];
+
+    // waits until the full content is uploaded. Always you use await, the superior hierarchical
+    // function must be async
+    for await(const chunk of req) { // traverse all stream
+        buffers.push(chunk);
+    }
+
+    try {
+        // executed after the await waits the full content is uploaded
+        // JSON.parse - transforms a string (with json format) in a json object
+        req.body = JSON.parse(Buffer.concat(buffers).toString()); // creates a property called body in req object
+    } catch {
+        req.body = null;
+    }
 
     if (method === 'GET' && url === '/users') {
         return res
@@ -13,10 +29,13 @@ const server = http.createServer((req, res) => {
     }
 
     if (method === 'POST' && url === '/users') {
+
+        const { name, email } = req.body;
+
         users.push({
             id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com'
+            name, // same as name = name or name = body.name
+            email // same as email = email or email = body.email
         })
         return res
             .writeHead(201)
